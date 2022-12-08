ea6df80f55707fea80a7bddecfaeb378d2c5ce6b
Daan <dws.vanbragt@student.avans.nl>
2022-12-08 09:24:00 +0100

    Moved childviewmodel to be a regular model

---
+++ SantasWishList.Data/Models/Child.cs
@@ -0,0 +1,20 @@
+﻿using SantasWishlist.Domain;
+
+namespace SantasWishList.Data.Models
+{
+    public class Child
+    {
+        public int Age { get; set; }
+        public Behaviour Behaviour { get; set; }
+        public bool HasBeenNice { get; set; }
+        public string? Reasoning { get; set; }
+        public Dictionary<GiftCategory, Gift>? Wishlist { get; set; }
+    }
+    
+    public enum Behaviour
+    {
+        BRAAF,
+        BEETJE,
+        STOUT
+    }
+}
