7d590a0c9dfedcaec83f8460ab95c2cac1b3c4f4
Daan <dws.vanbragt@student.avans.nl>
2022-12-08 09:48:38 +0100

    give child a user

--- SantasWishList.Data/Models/Child.cs
+++ SantasWishList.Data/Models/Child.cs
@@ -4,9 +4,9 @@ namespace SantasWishList.Data.Models
 {
     public class Child
     {
+        public User user { get; set; }
         public int Age { get; set; }
         public Behaviour Behaviour { get; set; }
-        public bool HasBeenNice { get; set; }
         public string? Reasoning { get; set; }
         public List<Gift>? Wishlist { get; set; }
     }
