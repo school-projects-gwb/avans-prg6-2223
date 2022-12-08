bfe791fb02441672e22b4cd24a1e8a5b414d1f27
jvanhouts <jvanhouts@protonmail.com>
2022-12-08 09:29:35 +0100

    update

---
+++ SantasWishList.Web/Models/ChildViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Build.Framework;
+using SantasWishlist.Domain;
+
+namespace SantasWishList.Web.Models
+{
+    public class ChildViewModel
+    {
+        [Required]
+        public int Age { get; set; }
+        [Required]
+        public Behaviour Behaviour { get; set; }
+        [Required]
+        public bool HasBeenNice { get; set; }
+        [Required]
+        public string Reasoning { get; set; }
+        [Required]
+        public Dictionary<GiftCategory, Gift> Wishlist { get; set; }
+    }
+    
+    public enum Behaviour
+    {
+        BRAAF,
+        BEETJE,
+        STOUT
+    }
+}
