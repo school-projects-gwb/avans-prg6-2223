5438be3ced5f30268c47f540d35d9889405607d9
jvanhouts <jvanhouts@protonmail.com>
2022-12-08 11:45:31 +0100

    fix

--- SantasWishList.Data/Models/Child.cs
+++ SantasWishList.Data/Models/Child.cs
@@ -4,6 +4,7 @@ namespace SantasWishList.Data.Models
 {
     public class Child
     {
+        public User User { get; set; }
         public int Age { get; set; }
         public Behaviour Behaviour { get; set; }
         public string? Reasoning { get; set; }
